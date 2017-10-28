2107f64f7706ee43f39a9070f3a9a390688b042b
Anik Hasan <anik.hasan@blackswan.com>
2017-10-28 13:35:33 +0600

    getting quill error

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ViewEncapsulation } from '@angular/core';
+import { FormGroup, AbstractControl, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,37 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  public form:FormGroup;
+  public content:AbstractControl;
+
+  public editorConfig = {
+    theme: 'bubble',
+    placeholder: "输入任何内容，支持html",
+    modules: {
+      toolbar: [
+        ['bold', 'italic', 'underline', 'strike'],
+        [{ 'list': 'ordered'}, { 'list': 'bullet' }],
+        [{ 'header': [1, 2, 3, 4, 5, 6, false] }],
+        [{ 'color': [] }, { 'background': [] }],
+        [{ 'font': [] }],
+        [{ 'align': [] }],
+        ['link', 'image'],
+        ['clean']
+      ]
+    }
+  };
+
+  constructor(private _fb:FormBuilder) {
+    this.form = _fb.group({
+      'content': ['<p>I am Example 01</p>', Validators.compose([Validators.required])],
+    });
+
+    this.content = this.form.controls['content'];
+  };
+
+  public submitAnnouncement(values:Object):void {
+    if (this.form.valid) {
+       console.log('Submit!', values);
+    }
+  }
 }
